d25ffb11441e2716f3cb05e655aff5dfcb4ada13
Yohanes Amadeo Marvell <127278310+amadeomarvell@users.noreply.github.com>
2023-12-16 11:23:38 +0700

    Update config.py
    update paths and classes

--- config.py
+++ config.py
@@ -3,21 +3,18 @@ from yacs.config import CfgNode
 
 _C = CfgNode()
 
-_C.DATASET_DIR = 'flickr_logos_27_dataset'
-_C.IMAGE_DIR = os.path.join(_C.DATASET_DIR, 'flickr_logos_27_dataset_images')
+_C.DATASET_DIR = 'Car_Logo_Dataset_27'
+_C.IMAGE_DIR = os.path.join(_C.DATASET_DIR, 'Car_Logo_Dataset_27_Images')
 _C.ANNOT_FILE = os.path.join(
-    _C.DATASET_DIR, 'flickr_logos_27_dataset_training_set_annotation.txt')
+    _C.DATASET_DIR, 'car_logos_27_dataset_training_set_annotation.txt')
 _C.CROPPED_ANNOT_FILE = os.path.join(
-    _C.DATASET_DIR, 'flickr_logos_27_dataset_training_set_annotation_cropped.txt')
+    _C.DATASET_DIR, 'car_logos_27_dataset_training_set_annotation_cropped.txt')
 _C.CROPPED_ANNOT_FILE_TEST = os.path.join(
-    _C.DATASET_DIR, 'flickr_logos_27_dataset_test_set_annotation_cropped.txt')
+    _C.DATASET_DIR, 'car_logos_27_dataset_test_set_annotation_cropped.txt')
 
-_C.CLASS_NAMES = [
-    'Adidas', 'Apple', 'BMW', 'Citroen', 'Cocacola', 'DHL', 'Fedex', 'Ferrari',
-    'Ford', 'Google', 'HP', 'Heineken', 'Intel', 'McDonalds', 'Mini', 'Nbc',
-    'Nike', 'Pepsi', 'Porsche', 'Puma', 'RedBull', 'Sprite', 'Starbucks',
-    'Texaco', 'Unicef', 'Vodafone', 'Yahoo'
-]
+_C.CLASS_NAMES = ['Audi', 'Citroen', 'GMC', 'Honda', 'Hyundai', 'Mazda', 'Mercedes', 'Mitsubishi', 
+                  'Nissan', 'Renault', 'Toyota', 'Volkswagen', 'bmw', 'cadillac', 'chevrolet', 'ford', 'jeep', 
+                  'kia', 'lexus', 'license-plate', 'mini', 'porsche', 'ram', 'range-rover', 'subaru', 'suzuki', 'volvo']
 
 
 def get_cfg_defaults():
